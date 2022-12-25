05d41e9f84c684d66763854b9b490d27736ab203
Nantawat Rattanaton <aoaoaoao538@gmail.com>
2022-12-25 15:55:58 +0700

    Move Style to Styles Folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.css';
 import { Link , Route , Routes } from 'react-router-dom'
 import styled from 'styled-components';
 import Home from './screens/Home';
