b63aff1feb5d9538e097e921b597fba729118591
Nantawat Rattanaton <aoaoaoao538@gmail.com>
2022-12-25 16:45:15 +0700

    Change Navbar Style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,20 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 export default function Navbar() {
   // Styled Component Section
-  const Navbar = styled.div`
-    background-color: white;
+  const Navbar = styled.h1`
+    background-color: #1a120b;
     color: #fffded;
     display: flex;
     position: relative;
     align-items: center;
+    justify-content: center;
     height: 60px;
     gap: 40px;
-    font-size: 1.2rem;
+    margin: 0;
   `;
   const linkStyle = {
-    margin: "5px",
     textDecoration: "none",
-    color: "blue",
+    color: "white",
   };
   return (
     <Navbar>
