bf59c3b9f5d04cb93a93581f0d551abdf3be78c0
Nantawat Rattanaton <aoaoaoao538@gmail.com>
2022-12-25 21:48:14 +0700

    Add new Page

---
+++ src/screens/Project1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Employees from "../components/components-project-1/Employees";
+
+export default function Project1() {
+  return (
+    <>
+      <div>Project1</div>
+      <Employees />
+    </>
+  );
+}
