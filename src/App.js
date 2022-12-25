f2c7c8a7bb08c11208867254323c9c10bc5a60a9
Nantawat Rattanaton <aoaoaoao538@gmail.com>
2022-12-25 13:03:21 +0700

    Test react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
-import { Route , Routes } from 'react-router-dom'
+import { Link , Route , Routes } from 'react-router-dom'
+import styled from 'styled-components';
+import Home from './screens/Home';
+import About from './screens/About';
+
 function App() {
+  const Navbar = styled.div`
+  background-color: aliceblue;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  `
+
   return (
-    <div>
-      Template for Starting React Project
-    </div>
+    <>
+      <Navbar>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
