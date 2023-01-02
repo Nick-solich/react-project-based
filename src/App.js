b38e07a57079708233ba9731ebd8551cf9df749e
Nantawat Rattanaton <aoaoaoao538@gmail.com>
2023-01-02 20:12:50 +0700

    Create Body Component

--- src/App.js
+++ src/App.js
@@ -5,17 +5,29 @@ import About from './screens/About';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Project1 from './screens/Projects';
+import styled from 'styled-components';
 
 function App() {
   // Variable Section
+  const Body = styled.body`
+  margin: 0;
+  padding: 0;
+  position: relative;
+  background-color: #E5E5CB;
+  box-sizing: border-box;
+  /* Applied From Navbar's height and Footer's height*/
+  min-height: calc(100vh - 120px);
+  `
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Project1 />} />
-      </Routes>
+      <Body>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Project1 />} />
+        </Routes>
+      </Body>
       <Footer />
     </>
   );
