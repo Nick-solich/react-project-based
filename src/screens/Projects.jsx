68271c9d1a4c8062977ab2649233abd4777742c2
Nantawat Rattanaton <aoaoaoao538@gmail.com>
2023-01-12 10:43:45 +0700

    🎒Change Tag in Projects screen

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -2,10 +2,5 @@ import React from "react";
 import Employees from "../components/components-project-1/Employees";
 
 export default function Projects() {
-  return (
-    <>
-      <h1>Projects</h1>
-      <Employees />
-    </>
-  );
+  return <Employees />;
 }
